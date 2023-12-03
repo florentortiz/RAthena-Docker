ec36efa2e4d3e0c6e4c8d32ca05319dd2cc95698
Mido <Maximebodartpro@gmail.com>
2023-12-03 14:14:40 +0100

    Ok cool : #2

---
+++ RagnarokDocker/server/defines_post.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) rAthena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef CONFIG_CUSTOM_DEFINES_POST_HPP
+#define CONFIG_CUSTOM_DEFINES_POST_HPP
+
+/**
+ * rAthena configuration file (http://rathena.org)
+ * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
+ **/
+
+#undef PACKET_OBFUSCATION
+#undef PACKET_OBFUSCATION_WARN
+
+#endif /* CONFIG_CUSTOM_DEFINES_POST_HPP */
--- RagnarokDocker/wsproxy/boot.sh
+++ RagnarokDocker/wsproxy/boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-./wsproxy -p 5999 -a ragnarok:6900,ragnarok:6121,ragnarok:5121
+wsproxy -p 5999 -a ragnarok:6900,ragnarok:6121,ragnarok:5121
 
 while [ 1 ]; do
     /bin/bash
